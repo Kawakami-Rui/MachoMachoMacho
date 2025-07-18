62eddd48a73933be04f3be3cc07e25634c92d656
Rui Kawakami <e245706@ie.u-ryukyu.ac.jp>
2025-07-18 19:05:06 +0900

    カレンダー,種目リスト,結合

--- models.py
+++ models.py
@@ -14,3 +14,19 @@ class Exercise(db.Model):
 
     def __str__(self):
         return f'種目ID：{self.id} 種目名：{self.name} カテゴリ：{self.category} 詳細：{self.detail}'
+    
+class WorkoutLog(db.Model):
+    __tablename__ = 'workout_logs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable=False)
+    sets = db.Column(db.Integer, nullable=True)
+    reps = db.Column(db.Integer, nullable=True)
+    weight = db.Column(db.Float, nullable=True)
+    category = db.Column(db.String(100), nullable=False)
+
+    exercise = db.relationship('Exercise', backref='logs')
+
+    def __str__(self):
+        return f'記録日：{self.date} 種目名：{self.exercise.name}（カテゴリ：{self.category}） {self.sets}セット×{self.reps}回 {self.weight}kg'
